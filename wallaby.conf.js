9b08a8c0c30971d1150e676add8f723808b37d68
Pavneet Singh Saund <pav.saund@gmail.com>
2019-11-26 08:34:56 +0100

    Add babel compiler for js files

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,3 +1,15 @@
 const build = require('@dolittle/typescript.build');
 
-module.exports = build.wallaby();
+module.exports = build.wallaby((wallaby, settings) => {
+    settings.compilers = {
+        '**/*.js': wallaby.compilers.babel(),
+        '**/*.@(js|ts)': wallaby.compilers.typeScript({
+            module: 'commonjs',
+            downlevelIteration: true,
+            allowJs: true,
+            experimentalDecorators: true,
+            esModuleInterop: true,
+            target: 'es6'
+        })
+    }
+});
