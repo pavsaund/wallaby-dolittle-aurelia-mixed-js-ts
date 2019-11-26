12bf0ab0e90dab764acba565e43eebf996a89dfe
Pavneet Singh Saund <pav.saund@gmail.com>
2019-11-26 11:12:00 +0100

    Add js file running as part of a spec

---
+++ Features/for_something/given/a_context.ts
@@ -0,0 +1,8 @@
+import { something } from '../../something';
+
+export class a_context {
+    setup_some_stuff: something
+    constructor() {
+        this.setup_some_stuff = new something('string', 1);
+    }
+}
